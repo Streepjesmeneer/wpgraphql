18b67019e9a7f95d6d245fe1eeb3975a4e1b7db3
krisvhes <vanhesk@gmail.com>
2023-05-19 16:10:43 +0200

    added demo route

--- src/Handlers/RouteHandler.php
+++ src/Handlers/RouteHandler.php
@@ -6,5 +6,15 @@ namespace WPgraphql\Handlers;
 
 class RouteHandler
 {
+    /**
+     * DEMO ROUTE
+     * Method: GET
+     * Endpoint: /products
+     */
+    public static function get_products()
+    {
+        $products = file_get_contents(WPG_PLUGIN_PATH . '/demo/products.json');
 
+        return wp_send_json(json_decode($products));
+    }
 }
