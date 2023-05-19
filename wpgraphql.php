544db43b7dbcc6b55fc58b3726f8b7b830115a6d
krisvhes <kris@socialbrothers.nl>
2023-05-19 12:15:21 +0200

    add comment to method

--- wpgraphql.php
+++ wpgraphql.php
@@ -20,6 +20,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 defined('ABSPATH') || exit('Forbidden');
 
+/**
+ * Check if WPGraphQL is active
+ * otherwise return error
+ */
 register_activation_hook(__FILE__, function(){
     if (!is_plugin_active('wp-graphql/wp-graphql.php')) {
         _e('ERROR: Plugin WPGraphQL needs to be active to this plugin to work', 'WPG');
