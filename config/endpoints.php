942c93476746d283b5894b621d88bb84b263c1bf
krisvhes <vanhesk@gmail.com>
2023-05-19 16:13:00 +0200

    added todo

--- config/endpoints.php
+++ config/endpoints.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 defined('ABSPATH') || exit('Forbidden');
 
 // TODO: make this configurable
+// TODO: return instead of variable
 $router_namespace = 'wpgraphql';
 
 $endpoints = [
