fa8c9309f675c426dd5f9a07ee18f88156cbb3c9
krisvhes <kris@socialbrothers.nl>
2023-05-19 10:27:24 +0200

    add beginning of the plugin base

---
+++ wpgraphql.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WPgraphql;
+
+/**
+ * @author  Kris van Hes <kris@socialbrothers.nl>
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WPgraphql demo
+ * Description:       Simple API request demo
+ * Version:           0.0.1
+ * Author:            Kris van Hes
+ * Update URI:        false
+ */
+
+defined('ABSPATH') || exit('Forbidden');
