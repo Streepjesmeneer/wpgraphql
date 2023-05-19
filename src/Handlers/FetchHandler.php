e7a7841112afd4467542d896c7f886ff2771a208
krisvhes <vanhesk@gmail.com>
2023-05-19 17:24:43 +0200

    add env files

--- src/Handlers/FetchHandler.php
+++ src/Handlers/FetchHandler.php
@@ -3,12 +3,23 @@
 declare(strict_types=1);
 
 namespace WPgraphql\Handlers;
+use Exception;
 use \GuzzleHttp\Client;
 
 class FetchHandler
 {
-    public function connect(): void
+    public static function connect(string $endpoint = ''): ?string
     {
+        $app    = $_ENV['APP_URL'] ?? '';
         $client = new Client();
+
+        try {
+            $response = $client->get(sprintf('%s/%s', $app, $endpoint));
+            $body = (string) $response->getBody();
+
+            return $body;
+        } catch(Exception $error) {
+            echo sprintf("Error: %s", $error->getMessage());
+        }
     }
 }
