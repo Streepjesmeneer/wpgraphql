1f083cec74112c9120df723c060d1a02253adb4c
krisvhes <kris@socialbrothers.nl>
2023-06-16 13:48:52 +0200

    finished svelte setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+// vite.config.js
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [svelte()],
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@scripts': resolve(__dirname, 'src/scripts'),
+    },
+  },
+});
